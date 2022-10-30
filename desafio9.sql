2ef7846c8ccff3dc9d283704b80c0625aa800880
karl-no <karl.publicidade@gmail.com>
2022-10-30 15:19:09 -0300

    Requisito 11

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,4 @@
 SELECT COUNT(h.id_cancao) AS quantidade_musicas_no_historico FROM SpotifyClone.historico_de_reproducoes AS h
 INNER JOIN SpotifyClone.usuario AS u
 ON h.usuario_id = u.usuario_id
--- GROUP BY u.usuario_id
--- GROUP BY h.usuario_id
 WHERE u.nome_usuario LIKE 'Barbara Liskov';
